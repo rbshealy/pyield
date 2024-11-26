8153242fac72e29f9915365d54ca35e76183778d
Ray Shealy <rbshealy227@gmail.com>
2024-11-25 19:12:05 -0500

    Add example file and stub out more methods

--- HyperBool.py
+++ HyperBool.py
@@ -1,5 +1,6 @@
-from argparse import ArgumentError
 from enum import Enum
+from random import randrange
+
 
 class HyperBool(Enum):
     TRUE = 1.0
@@ -10,36 +11,21 @@ class HyperBool(Enum):
     _concrete_vals = [TRUE, FALSE]
     _non_concrete_vals = [X, ERR]
 
-    def __init__(self, value):
-        if value.__class__ is bool:
-            self.value = HyperBool.eval(value)
-        elif value.__class__ is HyperBool:
-            self.value = value.value
-        elif value.__class__ is float and HyperBool.is_valid(value):
-            self.value = value
-        else:
-            raise ArgumentError(f"Argument {value} must be a valid HyperBool")
-
-
-        if value in HyperBool._concrete_vals:
-            self.is_concrete = True
-        else:
-            self.is_concrete = False
-
     def __repr__(self):
-        if self.value == HyperBool.TRUE:
-            return 'HyperBool.TRUE'
-        elif self.value == HyperBool.FALSE:
-            return 'HyperBool.FALSE'
-        elif self.value == HyperBool.X:
-            return 'HyperBool.X'
-        elif self.value == HyperBool.ERR:
-            return 'HyperBool.ERR'
+        return f"{self.__class__.__name__}({self.value!r})"
 
-        return NotImplementedError
+    def __str__(self):
+        return self.name
 
     def __bool__(self):
-        return NotImplemented
+        if self.value == HyperBool.TRUE:
+            return True
+        elif self.value == HyperBool.FALSE:
+            return False
+        elif self.value == HyperBool.X:
+            return randrange(0,2)
+        elif self.value == HyperBool.ERR:
+            raise ValueError("cannot evaluate HyperBool.Err as a boolean value")
 
     def __or__(self, other):
         return NotImplemented
@@ -51,10 +37,13 @@ class HyperBool(Enum):
         return NotImplemented
 
     def __add__(self, other):
+        self_concrete = self.value in HyperBool._concrete_vals
+        other_concrete = other.value in HyperBool._concrete_vals
+
         if self.__class__ is other.__class__:
-            if self.is_concrete and other.is_concrete:
+            if self_concrete and other_concrete:
                 return HyperBool.eval(bool(self.value) or bool(other.value))
-            elif self.is_concrete:
+            elif self_concrete:
                 if other.value == HyperBool.X:
                     if bool(self.value):
                         return HyperBool.TRUE
@@ -62,7 +51,7 @@ class HyperBool(Enum):
                         return HyperBool.X
                 else:
                     return HyperBool.ERR
-            elif other.is_concrete:
+            elif other_concrete:
                 if self.value == HyperBool.X:
                     if bool(other.value):
                         return HyperBool.TRUE
@@ -71,7 +60,10 @@ class HyperBool(Enum):
                 else:
                     return HyperBool.ERR
             else:
-                return NotImplemented
+                if self.value == HyperBool.ERR or other.value == HyperBool.ERR:
+                    return HyperBool.ERR
+                else:
+                    return HyperBool.X
         return NotImplemented
 
     def __mul__(self, other):
@@ -107,21 +99,23 @@ class HyperBool(Enum):
         return NotImplemented
 
 
-    @classmethod
-    def eval(cls, expression):
-        if expression:
-            return HyperBool.TRUE
-        else:
-            return HyperBool.FALSE
+    @staticmethod
+    def eval(expression):
+        if expression.__class__ is bool:
+            if expression:
+                return HyperBool.TRUE
+            else:
+                return HyperBool.FALSE
+        if expression.__class__ is float and HyperBool.is_valid(expression):
+            return expression
 
-    @classmethod
-    def is_valid(cls, value) -> bool:
-        if (value in HyperBool._concrete_vals or value in HyperBool._non_concrete_vals):
+    @staticmethod
+    def is_valid(value) -> bool:
+        if value in {HyperBool.TRUE, HyperBool.FALSE, HyperBool.X, HyperBool.ERR}:
             return True
         else:
             return False
 
-
-
-
-
+    @staticmethod
+    def print_table(left, op, right):
+        return NotImplemented
---
+++ pyield-example.py
@@ -0,0 +1,10 @@
+from HyperBool import HyperBool
+
+if __name__ == '__main__':
+
+    h_bool_1 = HyperBool.TRUE
+    h_bool_2 = HyperBool(True)
+
+
+    print(h_bool_1)
+    print(h_bool_2)
